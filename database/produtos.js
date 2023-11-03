dba27088f54ebedf8879bef206fb5ad86ead4afa
LacamJC <joaoramajo744@gmail.com>
2023-11-03 11:54:16 -0300

    AAAAAAAAAAAAA

--- database/produtos.js
+++ database/produtos.js
@@ -2,37 +2,6 @@
  const database = require('./conexao')
 
  // PRODUTOS.JS é a pagina de criação da tabela dos clientes mas mantera o nome produto.js
-
-
-// const Produto = database.define('produtos', {
-//     id_prod: {
-//         type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     primaryKey:true 
-//     },
-
-//     descricao_prod:{
-//         type: Sequelize.STRING(60),
-//         unique: true,
-//         allowNull: false
-//     },
-
-//     valor_unitario: {
-//         type:Sequelize.DECIMAL(6,2),
-//         allowNull: false
-//     },
-//     qtde_estoque:{
-//         type:Sequelize.INTEGER,
-//         allowNull:false
-//     },
-
-//     imagem_produto: {
-//              type: Sequelize.STRING(50),
-//          allowNull: false
-//      }
-// })
-
-
 const usuario = database.define('usuarios', {
     cod_usuario: {
         type: Sequelize.INTEGER,
@@ -67,16 +36,6 @@ const usuario = database.define('usuarios', {
         allowNull: true,
     },
 
-    // comple_end_usuario: {
-    //     type: Sequelize.STRING,
-    //     allowNull: true,
-    // },
-
-    // num_end_usuario: {
-    //     type: Sequelize.STRING,
-    //     allowNull: false, 
-    // },
-
     senha_usuario: {
         type: Sequelize.STRING,
         allowNull: false,
@@ -93,21 +52,10 @@ const usuario = database.define('usuarios', {
         allowNull: true,
     }
 
-    // status_usuario: {
-    //     type: Sequelize.INTEGER,
-    //     allowNull: true,
-    // }
-
-    // doc_usuario: {
-    //     type: Sequelize.STRING,
-    //     allowNull: false,
-    // },
-
-
 })
 
 
     
-// usuario.sync({force: true})
+usuario.sync({force: true})
 
  module.exports = usuario
