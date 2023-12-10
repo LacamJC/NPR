36e7a68753c4e109dd724adf0e88d954fee07925
LacamJC <joaoramajo744@gmail.com>
2023-12-10 16:11:03 -0300

    Tentando fazer o deploy

--- database/conexao.js
+++ database/conexao.js
@@ -1,8 +1,10 @@
 const Sequelize = require ('sequelize');
-const sequelize = new Sequelize('bdNpr', 'ramajo', '', {
+const sequelize = new Sequelize('railway', 'root', '4F-HdCd2C2hB22c66hAAdE-A1ff1d5hh', {
     dialect: 'mysql',
-    host: 'localhost',
-    port: 3306
+    host: 'viaduct.proxy.rlwy.net',
+    port: 58944
 })
 
 module.exports = sequelize
+
+//'bdNpr', 'ramajo', '', 
